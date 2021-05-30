b11c48d3ba6852d5969f45aa0bec6100312720d6
nowhereboy <1186158072@qq.com>
2021-05-30 14:15:00 +0800

    A: add LR1 parser-1.0

---
+++ LexicalAnalyzer.py
@@ -0,0 +1,95 @@
+# coding:utf-8
+
+import re
+from Symbol import *
+
+keywords = ['if', 'else', 'while', 'int', 'return', 'void']         # 关键字
+operator = ['+', '-', '*', '/', '<', '>', '<=', '>=', '!=', '=']    # 运算符
+delimiters = ['{', '}', '(', ')', ',', ';']                         # 界符
+
+class LexicalAnalyzer:
+
+    def __init__(self, filepath):
+        self.tokens = []          # 词法分析处理结果：标识符集合
+        self.filepath = filepath  # 源文件路径
+        self.source = ''          # 源文件内容
+        self.processed = ''       # 预处理后文件的内容
+
+    # 函数功能：预处理文件，删除注释
+    def preprocessing(self):
+        with open(self.filepath, 'r') as f:
+            self.source = f.read()
+        self.processed = self.source
+        # 将单行注释删除
+        comments = re.findall(r'//.*?\n', self.processed)
+        if len(comments) > 0:
+            for i in comments:
+                self.processed = self.processed.replace(i, "")
+        # 将多行注释删除，DOTALL表示 . 将匹配包括换行符的任意字符
+        comments = re.findall(r'/\*.*?\*/', self.processed, flags=re.DOTALL)
+        if len(comments) > 0:
+            for i in comments:
+                self.processed = self.processed.replace(i, "")
+        # 消除换行符
+        self.processed = self.processed.strip()
+
+    # 打印出预处理过后的文件
+    def display(self):
+        print('预处理后的源文件:\n%s' % self.processed)
+        print('词法分析器的结果:%s\n' % self.tokens)
+
+    # 词法分析器处理的主体的函数
+    def processing(self):
+        flag = True
+        self.preprocessing()                # 预处理
+        target = self.processed
+        # 给界符和运算符前加个空格,确保能正确分割字符串
+        results = re.findall(r'({|}|\(|\)|,|;|\+|-|\*|/|>=|<=|>|<|==|!=|=)', target)
+        for i in results:
+            target = target.replace(i, ' '+i+' ')
+        # 利用空格分组,但是最后总是有一个空格产生
+        self.tokens = re.split(r'\s+', target)
+        del self.tokens[len(self.tokens) - 1]
+        # 分好组后将标识符和数字替换成ID和NUM，并将其余信息录入到符号表中
+        ptr = 0
+        checklist = []                  # 存有符号表中已经录入的符号
+        for j in self.tokens:
+            # 替换标识符并录入到符号表中
+            if re.match(r'([a-zA-Z][a-zA-Z0-9_]*)', j) and j not in keywords:
+                if j not in checklist:
+                    SymbolTable.append(Symbol(j, 'ID'))
+                    checklist.append(j)
+                self.tokens[ptr] = 'ID'
+            # 替换数字并录入到符号表中
+            elif re.match(r'(\d+)', j):
+                if j not in checklist:
+                    SymbolTable.append(Symbol(j, 'NUM'))
+                    checklist.append(j)
+                self.tokens[ptr] = 'NUM'
+            # 将关键字录入到符号表中
+            elif j in keywords:
+                if j not in checklist:
+                    SymbolTable.append(Symbol(j, 'KEYWORD'))
+                    checklist.append(j)
+            # 将界符录入到符号表中
+            elif j in delimiters:
+                if j not in checklist:
+                    SymbolTable.append(Symbol(j, 'DELIMITER'))
+                    checklist.append(j)
+            # 将运算符录入到符号表中
+            elif j in operator:
+                if j not in checklist:
+                    SymbolTable.append(Symbol(j, 'OPERATOR'))
+                    checklist.append(j)
+            else:
+                print('存在非法字符:%s' % j)
+                flag = False
+                break
+            ptr += 1
+        return flag
+
+
+if __name__ == '__main__':
+    test = LexicalAnalyzer("../测试文件/code.txt")
+    if test.processing():
+        test.display()
---
+++ production.py
@@ -0,0 +1,78 @@
+# coding:utf-8
+
+# 产生式类：语法规则中
+#
+class production:
+
+    def __init__(self):
+        self.__isitem=False     # 默认为产生式
+        self.__left=''
+        self.__right=[]
+        # 我们并不需要把点真的表示出来，用个数字表示即可
+        # 其中0表示形如S->.A的项目
+        self.__dot=-1           # -1表示没有点，即产生式
+        self.__next=[]          # 下一字符
+
+    # 用于项目间的比较  __eq__:==
+    def __eq__(self,other):
+        return self.__isitem==other.__isitem and self.__left==other.__left and self.__dot==other.__dot and self.__right==other.__right and self.__next==other.__next
+
+    def set_left(self,left):
+        self.__left=left
+
+    def set_right(self,right):
+        self.__right=right
+
+    def set_dot(self,position):
+        self.__dot=position
+        self.__isitem=True
+
+    def set_next(self,Next):
+        self.__next=Next
+
+    def get_rightnum(self):
+        return len(self.__right)
+
+    def get_left(self):
+        return self.__left
+
+    def get_right(self):
+        return self.__right
+
+    def get_next(self):
+        return self.__next
+
+    def get_dotposition(self):
+        return self.__dot
+
+    def item2produc(self):
+        self.__dot=-1
+        self.__isitem=False
+        self.__next=[]
+
+    def show(self, fd):
+        fd.write('%s ->'%self.__left)
+        if self.__isitem:
+            length=len(self.__right)
+            i=0
+            while i<=length:
+                if i==self.__dot:
+                    fd.write('.')
+                if i!=length:
+                    fd.write(self.__right[i])
+                i+=1
+            if len(self.__next):
+                #print(' ,%s'%self.__next,end='')
+                fd.write(' ,')
+                for i in self.__next:
+                    fd.write('%s '%i)
+        else:
+            for i in self.__right:
+                fd.write(i)
+        fd.write('\n')    #相当于一个换行符
+
+    def isnull(self,nullsymbol):
+        return self.__right[0]==nullsymbol
+
+
+    
